8fec39cc769a79a5602c0ca6ecdf2b49e271d101
Nicholas Murray <nmurray@expensify.com>
2021-06-13 17:15:56 -0700

    Refactor to functional component

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -3,44 +3,41 @@ import { Link } from "react-router-dom";
 import {Icon, Message, Divider, Button, Container, Grid} from "semantic-ui-react";
 import {Context as AuthContext} from "../context/fhirAuth";
 
-class Redirect extends Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    //     const { state, getAccessToken, ToggleExampleVisibility } = useContext(AuthContext);
-    //     const fhirAuth = state.fhirAuth;
-    //     const fhirData = state.fhirData;
-    //     this.initializeSmart = this.initializeSmart.bind(this);
-    //     this.toggleExampleVisibility = this.toggleExampleVisibility.bind(this);
-    // }
-    //
-    // componentDidMount() {
-    //     this.props.getAccessToken(window.location.href);
-    // }
+const Redirect = () => {
+    const { state, getAccessToken, ToggleExampleVisibility } = useContext(AuthContext);
+    React.useEffect(() => {
+        getAccessToken(window.location.href);
+    }, []);
 
-    render() {
-        // let fhirAuth = this.fhirAuth;
-        //
-        // if (fhirAuth.error !== null) {
-        //     return (
-        //         <Container style={{ paddingTop: "10px" }}>
-        //             <Message negative icon>
-        //                 <Icon name="warning sign" />
-        //                 <Message.Content>
-        //                     <Message.Header>Error</Message.Header>
-        //                     <div>{fhirAuth.error.description}</div>
-        //                     <Divider />
-        //                     <Button basic as={Link} size="small" to="/">
-        //                         &lt; Home
-        //                     </Button>
-        //                 </Message.Content>
-        //             </Message>
-        //         </Container>
-        //     );
-        // }
-
-        return <Grid />;
+    if (state.error !== null) {
+        return (
+            <Container style={{ paddingTop: "10px" }}>
+                <Message negative icon>
+                    <Icon name="warning sign" />
+                    <Message.Content>
+                        <Message.Header>Error</Message.Header>
+                        <div>{state.error.description}</div>
+                        <Divider />
+                        <Button basic as={Link} size="small" to="/">
+                            &lt; Home
+                        </Button>
+                    </Message.Content>
+                </Message>
+            </Container>
+        );
     }
+
+    return (
+        <Container>
+            <Message>
+                <Message.Content>
+                    <Message.Header>Here in Redirect</Message.Header>
+                    <div>nothing to see here</div>
+                </Message.Content>
+            </Message>
+            <Grid />
+        </Container>
+    );
 }
 
 export default Redirect;
--- src/credentials.js
+++ src/credentials.js
@@ -3,4 +3,4 @@ export const FHIR_CLIENT_ID = '0689a8dc-7a1f-4e32-84be-6fc9809a373f';
 
 // Set to something like "https://localhost/fhir/epic/redirect".  If using HTTPS without a valid domain
 // name (e.g. localhost) then first visit the root URL to ignore the security notice.
-export const FHIR_REDIRECT_URI = 'https://localhost/fhir/epic/redirect';
+export const FHIR_REDIRECT_URI = 'https://steward-rx.herokuapp.com/fhir/epic/redirect';
